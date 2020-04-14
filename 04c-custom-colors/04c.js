7389d9df65ac7f1c06f3a0c8cc47f2907d6d9fa6
Henner S <henner.renardi@gmail.com>
2020-04-14 15:44:50 +0700

    Update 04c.js

--- 04c-custom-colors/04c.js
+++ 04c-custom-colors/04c.js
@@ -45,8 +45,7 @@ blocks.registerBlockType( 'wpbt/tut-04c', {
               label: 'Custom Color',
               value: atts.customColor,
               disableCustomColors: true,
-              // If you want to use default color from Palette, simply remove this argument
-              colors: [
+              colors: [ // If you want to use default color from Palette, simply remove this argument
                 { name: 'Red', slug: 'red', color: '#f44336' },
                 { name: 'Green', slug: 'green', color: '#4caf50' },
                 { name: 'Blue', slug: 'blue', color: '#2196f3' },
